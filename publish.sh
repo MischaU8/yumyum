ee08651827d0bfb3b22d8d0f73665df4723faaba
Mischa Untaga <99098079+MischaU8@users.noreply.github.com>
2023-07-18 10:46:43 +0200

    add gzip encoding

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+datasette publish vercel _data/yumyum.db \
+    --project yumyum \
+    --metadata metadata.yaml \
+    --static static:_static \
+    --install datasette-block-robots \
+    --install datasette-gzip \
+    --install datasette-json-html \
+    --install datasette-sitemap \
+    --install datasette-template-sql \
+    --install beautifulsoup4 \
+    --plugins-dir _plugins \
+    --template-dir _templates \
+    --public
